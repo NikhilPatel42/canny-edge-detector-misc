fdc98cde072c2aa0c8819323ab024d49889a58f9
NikhilPatel42 <nikhil.patel.inter@gmail.com>
2023-03-31 15:34:21 +0530

    removed some lizard files

--- filesys.cpp
+++ filesys.cpp
@@ -1,13 +1,33 @@
-#include <iostream>
 #include <filesystem>
+#include <iostream>
+#include <string>
+#include <sys/stat.h>
+ 
 namespace fs = std::filesystem;
-
+ 
 int main()
 {
-    // print all files from the current directory and below
-    for(auto& p: fs::recursive_directory_iterator(fs::current_path()))
-        std::cout << p.path() << '\n';
-    // print all files from the current directory
-    for(auto& p: fs::directory_iterator(fs::current_path()))
-        std::cout << p.path() << '\n';
+    // Path to the directory
+    std::string path
+        = "/home/school/canny-edge-detector-misc/lizard/";
+ 
+    // This structure would distinguish a file from a
+    // directory
+    struct stat sb;
+ 
+    // Looping until all the items of the directory are
+    // exhausted
+    for (const auto& entry : fs::directory_iterator(path)) {
+ 
+        // Converting the path to const char * in the
+        // subsequent lines
+        fs::path outfilename = entry.path();
+        std::string outfilename_str = outfilename.string();
+        const char* path = outfilename_str.c_str();
+ 
+        // Testing whether the path points to a
+        // non-directory or not If it does, displays path
+        if (stat(path, &sb) == 0 && !(sb.st_mode & S_IFDIR))
+            std ::cout << path << std::endl;
+    }
 }
