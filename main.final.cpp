3070dc5beda4a973abf879aaf040b20d9b563d2f
NikhilPatel42 <nikhil.patel.inter@gmail.com>
2023-04-03 17:07:43 +0530

    Update main.final.cpp

--- main.final.cpp
+++ main.final.cpp
@@ -1,14 +1,15 @@
 #include <bits/stdc++.h>
+#include <omp.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
+
 #include <cmath>
 #include <cstdint>
 #include <cstdlib>
 #include <iomanip>
 #include <iostream>
 #include <vector>
-#include <omp.h>
 
 using namespace std;
 
@@ -30,24 +31,12 @@ class PGM {
     void edges(int weak_threshold = 50, int strong_threshold = 200);
 };
 
-// clang-format off
+const int8_t Gx[] = {-1, 0, 1, -2, 0, 2, -1, 0, 1};
+const int8_t Gy[] = {1, 2, 1, 0, 0, 0, -1, -2, -1};
+const float Gaus3x3[] = {0.0625, 0.125,  0.0625, 0.125, 0.25,
+                         0.125,  0.0625, 0.125,  0.0625};
 
-    //Sobel
-    const int8_t Gx[] = {-1, 0, 1,
-                         -2, 0, 2,
-                         -1, 0, 1};
-
-    const int8_t Gy[] = { 1, 2, 1,
-                          0, 0, 0,
-                         -1,-2,-1};
-
-    //Gausian blur
-    //3 x 3 kernel
-    const float Gaus3x3[] =  {0.0625, 0.125, 0.0625, 0.125, 0.25, 0.125, 0.0625, 0.125, 0.0625};
-
-// clang-format on
-PGM::~PGM() {
-}
+PGM::~PGM() {}
 
 void PGM::init() {
     size_ = w_ * h_;
@@ -64,10 +53,11 @@ void PGM::edges(int weak_threshold, int strong_threshold) {
     vector<int> temp(size_, 0);
     int a[size_];
 
-    // gaussian filter
-    // get cobv from pixel
-    // copy to pixel
-    // at very end finally copy to pixel
+    /* gaussian filter
+     * get cobv from pixel
+     * copy to pixel
+     * at very end finally copy to pixel
+     */
 
     for (int x = 0; x < w_; x++) {
         for (int y = 0; y < h_; y++) {
@@ -102,10 +92,12 @@ void PGM::edges(int weak_threshold, int strong_threshold) {
     cout << end - start << " seconds\n";
     start = omp_get_wtime();
 
-    // apply sobel kernels
-    // get conv from temp
-    // apply on pixel after getting magnitude (sqrt(gx)^2+(gy)^2) 
-
+    /* apply sobel kernels
+     * get conv from temp
+     *
+     *
+     * apply on pixel after getting magnitude (sqrt(gx)^2+(gy)^2)
+     */
     offset_xy = 1;  // 3x3
     for (int x = offset_xy; x < w_ - offset_xy; x++) {
         for (int y = offset_xy; y < h_ - offset_xy; y++) {
@@ -141,8 +133,8 @@ void PGM::edges(int weak_threshold, int strong_threshold) {
                                               // theta : 0 - 2PI
                 theta = theta * (360.0 / (2.0 * M_PI));  // degrees
 
-                if ((theta <= 22.5 && theta >= -22.5) || (theta <= -157.5) ||
-                    (theta >= 157.5)) {
+                if ((theta <= 22.5 && theta >= -22.5) ||
+                    (theta <= -157.5 && theta >= 157.5)) {
                     segment = 1;  // "-"
                 } else if ((theta > 22.5 && theta <= 67.5) ||
                            (theta > -157.5 && theta <= -112.5)) {
@@ -168,9 +160,10 @@ void PGM::edges(int weak_threshold, int strong_threshold) {
 
     start = omp_get_wtime();
 
-    // local maxima: non maxima suppression
-    // get suppressed pixels from temp
-    // copy into pixel
+    /* local maxima: non maxima suppression
+     * get suppressed pixels from temp
+     * copy into pixel
+     */
 
     temp = pixel;
 
@@ -180,7 +173,8 @@ void PGM::edges(int weak_threshold, int strong_threshold) {
 
             switch (tes[pos]) {
                 case 1:
-                    if (temp[pos - 1] >= temp[pos] || temp[pos + 1] > temp[pos]) {
+                    if (temp[pos - 1] >= temp[pos] ||
+                        temp[pos + 1] > temp[pos]) {
                         pixel[pos] = 0;
                     }
                     break;
@@ -217,9 +211,10 @@ void PGM::edges(int weak_threshold, int strong_threshold) {
     cout << end - start << " seconds\n";
     start = omp_get_wtime();
 
-    // double threshold
-    // compare values from temp
-    // copy into pixels
+    /* double thresholding
+     * compare values from temp
+     * copy into pixels
+     */
     for (int x = 0; x < w_; x++) {
         for (int y = 0; y < h_; y++) {
             int src_pos = x + (y * w_);
@@ -240,14 +235,13 @@ void PGM::edges(int weak_threshold, int strong_threshold) {
 
     start = omp_get_wtime();
 
-    // edges with hysteresis
-    // prolly can't be parallelized, copy pixel vals into pixel
+    /* track edges with hysteresis
+     * prolly can't be parallelized, copy pixel vals into pixel
+     */
     for (int x = 1; x < w_ - 1; x++) {
         for (int y = 1; y < h_ - 1; y++) {
             int src_pos = x + (y * w_);
-            if (pixel[src_pos] == 255) {
-                pixel[src_pos] = 255;
-            } else if (pixel[src_pos] == 100) {
+            if (pixel[src_pos] == 100) {
                 if (pixel[src_pos - 1] == 255 || pixel[src_pos + 1] == 255 ||
                     pixel[src_pos - 1 - w_] == 255 ||
                     pixel[src_pos + 1 - w_] == 255 ||
@@ -258,111 +252,109 @@ void PGM::edges(int weak_threshold, int strong_threshold) {
                 } else {
                     pixel[src_pos] = 0;
                 }
-            } else {
-                pixel[src_pos] = 0;
             }
         }
+
+        write("./5.pgm");
+
+        end = omp_get_wtime();
+        cout << end - start << " seconds\n";
+
+        cout << "All files written." << endl;
     }
 
-    write("./5.pgm");
+    /**
+     * Opens the PGM image file for write.
+     * @param Path of the PGM image file being read.
+     **/
+    void PGM::write(std::string filename) {
+        ofstream writer(filename);
 
-    end = omp_get_wtime();
-    cout << end - start << " seconds\n";
+        writer << "P2\n";
+        writer << w_ << " " << h_ << "\n";
+        writer << "255\n";
 
-    cout << "All files written." << endl;
-}
+        // print in 17 pixel long rows.
+        int numval = 0;
+        int flag = 0;
+        int counter = 0;
+        for (int x = 0; x < w_; x++)
+            for (int y = 0; y < h_; y++) {
+                int pos = counter;
+                writer << +pixel.at(pos);
+                numval++;
+                if (numval % 17 == 0 && flag == 1)
+                    writer << "\n";
+                else
+                    writer << " ";
+                flag = 1;
+                counter++;
+            }
 
-/**
- * Opens the PGM image file for write.
- * @param Path of the PGM image file being read.
- **/
-void PGM::write(std::string filename) {
-    ofstream writer(filename);
+        writer << endl;
+        writer.close();
+    }
 
-    writer << "P2\n";
-    writer << w_ << " " << h_ << "\n";
-    writer << "255\n";
-
-    // print in 17 pixel long rows.
-    int numval = 0;
-    int flag = 0;
-    int counter = 0;
-    for (int x = 0; x < w_; x++)
-        for (int y = 0; y < h_; y++) {
-            int pos = counter;
-            writer << +pixel.at(pos);
-            numval++;
-            if (numval % 17 == 0 && flag == 1)
-                writer << "\n";
-            else
-                writer << " ";
-            flag = 1;
-            counter++;
-        }
-
-    writer << endl;
-    writer.close();
-}
-
-/**
- * Opens the PGM image file for reading.
- * Sets: int w_, int h_, size_, std::vector<u_int8_t> pixel
- * @param Path of the PGM image file being read.
- **/
-void PGM::open(std::string filename) {
-    ifstream file;
-    file.exceptions(ifstream::badbit);  // attach badbit exception (hacky cpp
-                                        // exception attachment)
-    string read;
-    try {
-        file.open(filename, ios::in);
-        file >> read;
-        if (read.compare("P2") == 0) {
-            file >> w_ >> h_;
-            init();
-            pixel.resize(size_);
+    /**
+     * Opens the PGM image file for reading.
+     * Sets: int w_, int h_, size_, std::vector<u_int8_t> pixel
+     * @param Path of the PGM image file being read.
+     **/
+    void PGM::open(std::string filename) {
+        ifstream file;
+        file.exceptions(ifstream::badbit);  // attach badbit exception (hacky
+                                            // cpp exception attachment)
+        string read;
+        try {
+            file.open(filename, ios::in);
             file >> read;
-            int flag = 0;
-            if (read == "255") {
-                int counter = 0;
-                int numval = 0;
-                for (int x = 0; x < w_; x++)
-                    for (int y = 0; y < h_; y++) {
-                        file >> read;
-                        int pos = counter;
-                        pixel.at(pos) = stoi(read);
-                        cout << +pixel.at(pos);
-                        numval++;
-                        if (numval % 17 == 0 && flag == 1) flag = 1;
-                        counter++;
-                    }
-                cout << w_ << " " << h_ << " " << size_ << endl;
-                cout << "\nFile read." << endl;
+            if (read.compare("P2") == 0) {
+                file >> w_ >> h_;
+                init();
+                pixel.resize(size_);
+                file >> read;
+                int flag = 0;
+                if (read == "255") {
+                    int counter = 0;
+                    int numval = 0;
+                    for (int x = 0; x < w_; x++)
+                        for (int y = 0; y < h_; y++) {
+                            file >> read;
+                            int pos = counter;
+                            pixel.at(pos) = stoi(read);
+                            cout << +pixel.at(pos);
+                            numval++;
+                            if (numval % 17 == 0 && flag == 1) flag = 1;
+                            counter++;
+                        }
+                    cout << w_ << " " << h_ << " " << size_ << endl;
+                    cout << "\nFile read." << endl;
+                } else {
+                    string err{"file format error, not 8-bit!"};
+                    err.append(filename);
+                }
+
             } else {
-                string err{"file format error, not 8-bit!"};
+                string err{
+                    "file format error, not a pgm file(plain or otherwise)"};
                 err.append(filename);
             }
-
-        } else {
-            string err{"file format error, not a pgm file(plain or otherwise)"};
+        } catch (const ifstream::failure& e) {
+            string err{"Read error?! Check read permissions!"};
             err.append(filename);
+            std::cerr << err;
         }
-    } catch (const ifstream::failure& e) {
-        string err{"Read error?! Check read permissions!"};
-        err.append(filename);
-        std::cerr << err;
+        file.close();
     }
-    file.close();
-}
 
-int main() {
-    double start, end;
-    start = omp_get_wtime();
-    PGM opened;
-    opened.open("./01.pgm");
-    opened.edges();
-    cout << "succesfully exited";
-    end = omp_get_wtime();
-    cout << "Total time " << end - start << " seconds.\n";
-    return 0;
-}
+    int main() {
+        double start, end;
+        start = omp_get_wtime();
+        PGM opened;
+        opened.open("./01.pgm");
+        opened.edges();
+        cout << "succesfully exited";
+        end = omp_get_wtime();
+        cout << "Total time " << end - start << " seconds.\n";
+        return 0;
+    }
